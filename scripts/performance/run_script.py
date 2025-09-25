9cf76d93426191ae70780ad3150c3e0b75e932ab
Ananth Subramaniam <ananth.subramaniam@gmail.com>
2025-09-25 13:58:08 -0700

    Update scripts/performance/run_script.py
    Signed-off-by: Ananth Subramaniam <ananth.subramaniam@gmail.com>

--- scripts/performance/run_script.py
+++ scripts/performance/run_script.py
@@ -140,6 +140,7 @@ def main():
     pretrain(config=recipe, forward_step_func=forward_step)
 
     if torch.distributed.is_initialized():
+        torch.distributed.barrier()
         torch.distributed.destroy_process_group()
 
 
