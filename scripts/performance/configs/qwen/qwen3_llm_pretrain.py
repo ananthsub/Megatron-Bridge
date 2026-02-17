feb986b6021f2068a35c87fcd3539f2669da75a3
oliver könig <okoenig@nvidia.com>
2026-02-17 11:02:13 +0100

    fix: Set flex and alltoall for perf jobs (#2393)
    Signed-off-by: oliver könig <okoenig@nvidia.com>

--- scripts/performance/configs/qwen/qwen3_llm_pretrain.py
+++ scripts/performance/configs/qwen/qwen3_llm_pretrain.py
@@ -78,6 +78,7 @@ def qwen3_235b_a22b_pretrain_config_gb300(
     cfg.mixed_precision = precision_config
     cfg.comm_overlap = CommOverlapConfig(tp_comm_overlap=True)
     cfg.model.moe_flex_dispatcher_backend = base_cfg.moe_flex_dispatcher_backend
+    cfg.model.moe_token_dispatcher_type = "flex"
 
     set_qwen3_common_configs(cfg)
     set_workload_base_configs(cfg, base_cfg)
@@ -103,6 +104,7 @@ def qwen3_235b_a22b_pretrain_config_gb200(
     cfg.mixed_precision = precision_config
     cfg.comm_overlap = CommOverlapConfig(tp_comm_overlap=True)
     cfg.model.moe_flex_dispatcher_backend = base_cfg.moe_flex_dispatcher_backend
+    cfg.model.moe_token_dispatcher_type = "flex"
 
     set_qwen3_common_configs(cfg)
     set_workload_base_configs(cfg, base_cfg)
@@ -206,6 +208,7 @@ def qwen3_30b_a3b_pretrain_config_gb300(
     cfg.mixed_precision = precision_config
     cfg.comm_overlap = CommOverlapConfig(tp_comm_overlap=True)
     cfg.model.moe_flex_dispatcher_backend = base_cfg.moe_flex_dispatcher_backend
+    cfg.model.moe_token_dispatcher_type = "flex"
 
     set_qwen3_common_configs(cfg)
     set_workload_base_configs(cfg, base_cfg)
@@ -231,6 +234,7 @@ def qwen3_30b_a3b_pretrain_config_gb200(
     cfg.mixed_precision = precision_config
     cfg.comm_overlap = CommOverlapConfig(tp_comm_overlap=True)
     cfg.model.moe_flex_dispatcher_backend = base_cfg.moe_flex_dispatcher_backend
+    cfg.model.moe_token_dispatcher_type = "flex"
 
     set_qwen3_common_configs(cfg)
     set_workload_base_configs(cfg, base_cfg)
@@ -256,6 +260,7 @@ def qwen3_30b_a3b_pretrain_config_b300(
     cfg.mixed_precision = precision_config
     cfg.comm_overlap = CommOverlapConfig(tp_comm_overlap=True)
     cfg.model.moe_flex_dispatcher_backend = base_cfg.moe_flex_dispatcher_backend
+    cfg.model.moe_token_dispatcher_type = "flex"
 
     set_qwen3_common_configs(cfg)
     set_workload_base_configs(cfg, base_cfg)
